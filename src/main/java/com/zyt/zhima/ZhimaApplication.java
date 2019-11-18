4b114fd2024e529b76d5d6e5e2c6f323ebbe1911
e <18112929060@163.com>
2019-11-18 11:34:55 +0800

    zhima初始化

---
+++ src/main/java/com/zyt/zhima/ZhimaApplication.java
@@ -0,0 +1,13 @@
+package com.zyt.zhima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhimaApplication.class, args);
+	}
+
+}
